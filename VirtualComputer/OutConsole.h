319ed49715144967404fe5dd340142b533ae0af3
Hazurl <ricoujules69@gmail.com>
2017-09-22 19:09:02 +0200

    OutConsole

--- VirtualComputer/OutConsole.h
+++ VirtualComputer/OutConsole.h
@@ -6,15 +6,57 @@
 
 BEGIN_NS
 
+enum class OutConsoleMode {
+	Binary,
+	Int, 
+	Char
+};
+
+enum class OutConsoleBetween {
+	Space,
+	Endl,
+	Nothing
+};
+
+template<OutConsoleMode mode, OutConsoleBetween between>
 class OutConsole : public Processable {
 public:
-	OutConsole(Bus* bus_data);
 
-	void process() override;
+	OutConsole(Bus* bus_data) : bus_data(bus_data) {}
+
+	void OutConsole::process() {
+		byte data = bus_data->extract();
+		switch(mode) {
+		case vphaz::OutConsoleMode::Binary:
+			for(byte i = 7; i >= 0; --i)
+				std::cout << ((data & (1 << i)) != 0);
+			break;
+		case vphaz::OutConsoleMode::Int:
+			std::cout << (int)data;
+			break;
+		case vphaz::OutConsoleMode::Char:
+			std::cout << (char)data;
+			break;
+		}
+		
+		switch(between) {
+		case vphaz::OutConsoleBetween::Space:
+			std::cout << " ";
+			break;
+		case vphaz::OutConsoleBetween::Endl:
+			std::cout << std::endl;
+			break;
+		case vphaz::OutConsoleBetween::Nothing:
+			break;
+		}
+	}
 
 private:
 
 	Bus* bus_data;
 };
+
+using OutConsoleChar = OutConsole<OutConsoleMode::Char, OutConsoleBetween::Nothing>;
+using OutConsoleBinary= OutConsole<OutConsoleMode::Binary, OutConsoleBetween::Endl>;
 
 END_NS
